092d62440b148cb2d8a972dee45ff4e8a39ab399
Victor Capilla Borrego <victor.capilla@gmail.com>
2018-05-24 11:25:43 +0200

    - Carthage Added

---
+++ Carthage/Build/iOS/AlertViewVD.framework/Headers/AlertViewVD.h
@@ -0,0 +1,19 @@
+//
+//  AlertViewVD.h
+//  AlertViewVD
+//
+//  Created by Victor Capilla Borrego on 24/5/18.
+//  Copyright © 2018 Victor Capilla Borrego. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlertViewVD.
+FOUNDATION_EXPORT double AlertViewVDVersionNumber;
+
+//! Project version string for AlertViewVD.
+FOUNDATION_EXPORT const unsigned char AlertViewVDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlertViewVD/PublicHeader.h>
+
+
